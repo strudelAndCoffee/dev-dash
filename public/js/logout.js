018c7c5fd093adccb80ff2540e7cba7c55299e74
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-04-14 17:58:44 -0500

    logout button ends session and returns to homepage

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+async function logoutBtnHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch('/api/users/logout', {
+        method: 'POST'
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logoutBtnHandler);
